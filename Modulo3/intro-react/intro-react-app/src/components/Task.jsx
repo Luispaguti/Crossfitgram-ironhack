cec7599993ba9c2ab95d31ec0d13a9869538b79a
Luispaguti <l.palaciosgutierrez@gmail.com>
2022-11-24 22:39:41 +0100

    random task

---
+++ Modulo3/intro-react/intro-react-app/src/components/Task.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Task({name}) {
+  return (
+    <li className='list-group-item'>{name}</li>
+  )
+}
+
+export default Task;
--- Modulo3/intro-react/intro-react-app/src/components/TaskList.jsx
+++ Modulo3/intro-react/intro-react-app/src/components/TaskList.jsx
@@ -1,18 +1,33 @@
+import Task from "./Task"
+import { useState } from "react"
+
 const tasks = ["task 1", "task 2", "task 3", "task 4", "task ERROR",]
 
 
 function TaskList() {
+  const [list, setList] = useState([])
 
-return (
-  <div className="container">
-    <ul className="list-group">
-      {tasks.map((task, i) => (
-        <li key={i} className="list-group-item">
-          {task}
-          </li>))}
-    </ul>
-  </div>
-)
+  const handleClick = () => {
+    const randomTask = tasks[Math.floor(Math.random() * tasks.length)];
+
+    setList([randomTask, ...list ]);
+    
+  };
+
+  return (
+    <div className="container">
+      <button onClick={handleClick}  className="mb-2 btn btn-sm btn-primary">Add task</button>
+      {list.length === 0 ? (
+        <div className="alert alert-info">no tasks</div>
+      ) : (
+        <ul className="list-group">
+          {list.map((task, i) => (
+            <Task key={i} name={task} />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default TaskList
